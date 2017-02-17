2e6454c1cdad077b3b4facd0eb0d59f972fca315
zaddan <behzadboro@gmail.com>
2017-02-17 12:49:26 -0600

    blah

---
+++ int_ops_apx/src/v_src/unconfig_int_add__structure_preserved.v
@@ -0,0 +1,26 @@
+
+module unconfig_int_add ( clk, rst, a, b, c
+ );
+  input [31:0] a; 
+  input [31:0] b;
+  output [31:0] c;
+  input clk, rst;
+
+  wire a_not, b_not; 
+  
+  // --- F: forces the HDL to not destroy the logic (or rather optimize) 
+  // synopsys dc_script_begin
+  // set_dont_touch a_not
+  // set_dont_touch b_not
+  // synopsys dc_script_end
+  
+  //--- note the following can be structural or behavioral 
+  assign a_not = ~a[0];
+  assign b_not = ~b[0];
+  assign c[0] = a_not & b_not;
+//  not(a_not, a[0]);
+//  not(b_not, b[0]);
+//  and(c[0], a_not, b_not);
+
+  endmodule
+
